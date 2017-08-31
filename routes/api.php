2929d2a163bdd8b0723b8d4e09cd8fa7b7c48a1c
vagrant <vagrant@blogger>
2017-08-31 15:08:33 +0000

    We set our API app routes and our restful controllers

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,25 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+  //  return $request->user();
+//});
+
+
+Route::group(['prefix' => 'api/v1'], function() {// Here we group all routes which has same prefix
+    Route::resource('meeting', 'MeetingController', [ //Here we are creating meeting resource routes except edit and create
+        'except' => ['edit', 'create'] // Except this routes
+    ]);
+
+    Route::resource('meeting/registration', 'RegistrationController', [ //Here we are creating meeting resource controller routes except edit and create
+        'only' => ['store', 'destroy'] // only this routes
+    ]);
+
+    Route::post('user', [ //These are our routes for authentication and we create it in default way
+      'uses' => 'AuthController@store'
+    ]);
+
+    Route::post('user/signin', [ //These are our routes for authentication and we create it in default way
+      'uses' => 'AuthController@signin'
+    ]);
 });
